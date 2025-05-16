d47b844d7a92d037b2b64b58b8f9bce93254face
Chima <chimaokeke2006@gmail.com>
2025-05-16 13:21:58 +0100

    additional fixes

--- lib/screens/dashboard/home_screen.dart
+++ lib/screens/dashboard/home_screen.dart
@@ -39,7 +39,7 @@ class HomeScreen extends HookWidget {
             ),
             SizedBox(height: 25.h),
             SizedBox(
-              height: 150.h,
+              height: 130.h,
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 shrinkWrap: true,
@@ -106,50 +106,105 @@ class HomeScreen extends HookWidget {
                       children: [
                         Row(
                           children: [
-                            Text(
-                              'Available Cash',
-                              style: context.textTheme.bodyMedium!.copyWith(
-                                fontFamily: 'CircularStd',
-                                color: Color(0XFF4F5877),
-                              ),
-                            ),
-                            SizedBox(width: 12.w),
-                            Icon(Icons.visibility_off),
-                            Spacer(),
-                            GestureDetector(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Color(0XFF4D84FF),
-                                  borderRadius: BorderRadius.circular(4),
-                                ),
-
-                                child: Padding(
-                                  padding: const EdgeInsets.all(7.0),
-                                  child: Row(
+                            _Tokens(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset('assets/images/jpegs/breeze.png'),
+                                  SizedBox(height: 3.h),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
                                     children: [
-                                      Icon(Icons.add, color: Colors.white),
+                                      SvgPicture.asset(
+                                        'assets/images/svgs/coin.svg',
+                                      ),
+                                      SizedBox(width: 2.w),
                                       Text(
-                                        'Add to Wallet',
+                                        '100',
                                         style: context.textTheme.bodyMedium!
                                             .copyWith(
                                               fontFamily: 'CircularStd',
+                                              color: Color(0XFF001140),
                                             ),
                                       ),
                                     ],
                                   ),
-                                ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(width: 10.w),
+                            _Tokens(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset('assets/images/jpegs/flcn.png'),
+                                  SizedBox(height: 3.h),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      SvgPicture.asset(
+                                        'assets/images/svgs/coin.svg',
+                                      ),
+                                      SizedBox(width: 2.w),
+                                      Text(
+                                        '130',
+                                        style: context.textTheme.bodyMedium!
+                                            .copyWith(
+                                              fontFamily: 'CircularStd',
+                                              color: Color(0XFF001140),
+                                            ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(width: 10.w),
+                            _Tokens(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset('assets/images/jpegs/on.png'),
+                                  SizedBox(height: 3.h),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      SvgPicture.asset(
+                                        'assets/images/svgs/coin.svg',
+                                      ),
+                                      SizedBox(width: 2.w),
+                                      Text(
+                                        '100',
+                                        style: context.textTheme.bodyMedium!
+                                            .copyWith(
+                                              fontFamily: 'CircularStd',
+                                              color: Color(0XFF001140),
+                                            ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
                             ),
                           ],
                         ),
-                        SizedBox(height: 4.h),
-                        Text(
-                          '\$0.00',
-                          style: context.textTheme.displayMedium!.copyWith(
-                            fontFamily: 'CircularStd',
-                            color: Color(0XFF001140),
-                            fontWeight: FontWeight.w400,
-                          ),
+                        SizedBox(height: 20),
+                        Row(
+                          children: [
+                            Text(
+                              'View all my tokens',
+                              style: context.textTheme.bodyMedium!.copyWith(
+                                fontFamily: 'CircularStd',
+                                color: Color(0Xff4D84FF),
+                              ),
+                            ),
+                            SizedBox(width: 4.w),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              size: 14,
+                              color: Color(0XFF4D84FF),
+                            ),
+                          ],
                         ),
                       ],
                     ),
@@ -166,7 +221,7 @@ class HomeScreen extends HookWidget {
 
 class _BalanceContainer extends StatelessWidget {
   const _BalanceContainer({super.key, required this.child});
-  final Widget? child;
+  final Widget child;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -175,7 +230,26 @@ class _BalanceContainer extends StatelessWidget {
         color: Color(0XFFE1E6F0),
         borderRadius: BorderRadius.circular(4.r),
       ),
-      child: Padding(padding: const EdgeInsets.all(20.0), child: child),
+      child: Padding(padding: const EdgeInsets.all(15.0), child: child),
+    );
+  }
+}
+
+class _Tokens extends StatelessWidget {
+  const _Tokens({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60.h,
+      width: 90.w,
+      decoration: BoxDecoration(
+        color: Color(0XFFF0F4FD),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Padding(padding: const EdgeInsets.all(5.0), child: child),
     );
   }
 }
